20c0fcc10dd60f43e3f2ff5c0cb011cb8ab6abc4
Kanoonap <farazj105@gmail.com>
2024-04-19 16:33:27 +0500

    all changes done

--- lib/screens/chat/chats.dart
+++ lib/screens/chat/chats.dart
@@ -34,170 +34,172 @@ class _MessageViewState extends State<MessageView> {
             ),
           ),
         ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: Get.width * 0.024, vertical: Get.height * 0.01),
-          child: Column(
-            children: [
-              // Center(
-              //   child: Text(
-              //     'Recent',
-              //     style: kBody1MediumBlue,
-              //   ),
-              // ),
-              11.heightBox,
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.grey),
-                ),
-                child: TextFormField(
-                    controller: searchController,
-                    cursorColor: Colors.red,
-                    decoration: InputDecoration(
-                      hintText: 'Search for donors',
-                      border: InputBorder.none,
-                      prefixIcon: (searchText.isEmpty)
-                          ? const Icon(
-                              Icons.search,
-                              color: Colors.red,
-                            )
-                          : IconButton(
-                              icon: const Icon(
-                                Icons.clear,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: Get.width * 0.024, vertical: Get.height * 0.01),
+            child: Column(
+              children: [
+                // Center(
+                //   child: Text(
+                //     'Recent',
+                //     style: kBody1MediumBlue,
+                //   ),
+                // ),
+                11.heightBox,
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextFormField(
+                      controller: searchController,
+                      cursorColor: Colors.red,
+                      decoration: InputDecoration(
+                        hintText: 'Search for donors',
+                        border: InputBorder.none,
+                        prefixIcon: (searchText.isEmpty)
+                            ? const Icon(
+                                Icons.search,
                                 color: Colors.red,
+                              )
+                            : IconButton(
+                                icon: const Icon(
+                                  Icons.clear,
+                                  color: Colors.red,
+                                ),
+                                onPressed: () {
+                                  searchText = '';
+                                  searchController.clear();
+                                  setState(() {});
+                                },
                               ),
-                              onPressed: () {
-                                searchText = '';
-                                searchController.clear();
-                                setState(() {});
-                              },
-                            ),
-                      hintStyle: const TextStyle(
-                        fontSize: 14,
+                        hintStyle: const TextStyle(
+                          fontSize: 14,
+                        ),
                       ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        searchText = value;
-                      });
-                    }),
-              ),
-
-              const SizedBox(height: 10),
-              StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('chats')
-                      .orderBy('timeStamp', descending: true)
-                      .snapshots(),
-                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                          child: Padding(
-                        padding: EdgeInsets.only(top: 255),
-                        child: CircularProgressIndicator(),
-                      ));
-                    }
-                    if (!snapshot.hasData) {
-                      return Container();
-                    }
-                    QuerySnapshot chatSnapshot = snapshot.data as QuerySnapshot;
-                    return ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: chatSnapshot.docs.length,
-                        itemBuilder: (context, index) {
-                          final chatData = snapshot.data?.docs[index].data()
-                              as Map<String, dynamic>;
-                          dynamic group = chatData['group'];
-                          List<dynamic> groupIds = group.toList();
-
-                          String targetUserId1 = groupIds[0];
-                          String targetUserId2 =
-                              groupIds.length > 1 ? groupIds[1] : "";
-                          groupIds
-                              .remove(FirebaseAuth.instance.currentUser!.uid);
-                          return targetUserId1 ==
-                                      FirebaseAuth.instance.currentUser!.uid ||
-                                  targetUserId2 ==
-                                      FirebaseAuth.instance.currentUser!.uid
-                              ? FutureBuilder(
-                                  future: FirebaseFirestore.instance
-                                      .collection('users')
-                                      .doc(groupIds[0])
-                                      .get(),
-                                  builder: (context, userData) {
-                                    if (userData.hasError ||
-                                        !userData.hasData ||
-                                        !userData.data!.exists) {
-                                      return const SizedBox();
-                                    }
-                                    final targetUser = userData.data;
-                                    if (targetUser == null) {
-                                      return const SizedBox();
-                                    }
-                                    return Card(
-                                      shadowColor: Colors.black,
-                                      color: Colors.white,
-                                      elevation: 13,
-                                      child: Container(
-                                        padding: const EdgeInsets.only(left: 6),
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius:
-                                              BorderRadius.circular(14),
-                                        ),
-                                        child: ListTile(
-                                          onTap: () {
-                                            Get.to(() => ChatScreen(
-                                                  fcmToken:
-                                                      targetUser['fcmToken'],
-                                                  name: targetUser['username'],
-                                                  image: targetUser['image'],
-                                                  uid: targetUser['userId'],
-                                                  groupId: FirebaseAuth.instance
-                                                      .currentUser!.uid,
-                                                ));
-                                          },
-                                          contentPadding: EdgeInsets.zero,
-                                          leading: CircleAvatar(
-                                            radius: 30,
-                                            backgroundImage: NetworkImage(
-                                                targetUser['image']),
-                                          ),
-                                          title: Text(
-                                            targetUser['username'],
-                                            style: const TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 15,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          subtitle: Text(
-                                            //  encrypter?.decrypt64(chatData['lastMessage'], iv: iv).toString()??'';
-
-                                            chatData['lastMessage'],
-                                            style: const TextStyle(
-                                              fontSize: 12,
-                                            ),
-                                          ),
-                                          trailing: Padding(
-                                            padding: const EdgeInsets.only(
-                                              right: 10,
-                                            ),
-                                            child: Text(
-                                              chatData['timeStamp'],
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  })
-                              : const SizedBox();
+                      onChanged: (value) {
+                        setState(() {
+                          searchText = value;
                         });
-                  }),
-            ],
+                      }),
+                ),
+          
+                const SizedBox(height: 10),
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('chats')
+                        .orderBy('timeStamp', descending: true)
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                            child: Padding(
+                          padding: EdgeInsets.only(top: 255),
+                          child: CircularProgressIndicator(),
+                        ));
+                      }
+                      if (!snapshot.hasData) {
+                        return Container();
+                      }
+                      QuerySnapshot chatSnapshot = snapshot.data as QuerySnapshot;
+                      return ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: chatSnapshot.docs.length,
+                          itemBuilder: (context, index) {
+                            final chatData = snapshot.data?.docs[index].data()
+                                as Map<String, dynamic>;
+                            dynamic group = chatData['group'];
+                            List<dynamic> groupIds = group.toList();
+          
+                            String targetUserId1 = groupIds[0];
+                            String targetUserId2 =
+                                groupIds.length > 1 ? groupIds[1] : "";
+                            groupIds
+                                .remove(FirebaseAuth.instance.currentUser!.uid);
+                            return targetUserId1 ==
+                                        FirebaseAuth.instance.currentUser!.uid ||
+                                    targetUserId2 ==
+                                        FirebaseAuth.instance.currentUser!.uid
+                                ? FutureBuilder(
+                                    future: FirebaseFirestore.instance
+                                        .collection('users')
+                                        .doc(groupIds[0])
+                                        .get(),
+                                    builder: (context, userData) {
+                                      if (userData.hasError ||
+                                          !userData.hasData ||
+                                          !userData.data!.exists) {
+                                        return const SizedBox();
+                                      }
+                                      final targetUser = userData.data;
+                                      if (targetUser == null) {
+                                        return const SizedBox();
+                                      }
+                                      return Card(
+                                        shadowColor: Colors.black,
+                                        color: Colors.white,
+                                        elevation: 13,
+                                        child: Container(
+                                          padding: const EdgeInsets.only(left: 6),
+                                          decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:
+                                                BorderRadius.circular(14),
+                                          ),
+                                          child: ListTile(
+                                            onTap: () {
+                                              Get.to(() => ChatScreen(
+                                                    fcmToken:
+                                                        targetUser['fcmToken'],
+                                                    name: targetUser['username'],
+                                                    image: targetUser['image'],
+                                                    uid: targetUser['userId'],
+                                                    groupId: FirebaseAuth.instance
+                                                        .currentUser!.uid,
+                                                  ));
+                                            },
+                                            contentPadding: EdgeInsets.zero,
+                                            leading: CircleAvatar(
+                                              radius: 30,
+                                              backgroundImage: NetworkImage(
+                                                  targetUser['image']),
+                                            ),
+                                            title: Text(
+                                              targetUser['username'],
+                                              style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            subtitle: Text(
+                                              //  encrypter?.decrypt64(chatData['lastMessage'], iv: iv).toString()??'';
+          
+                                              chatData['lastMessage'],
+                                              style: const TextStyle(
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                            trailing: Padding(
+                                              padding: const EdgeInsets.only(
+                                                right: 10,
+                                              ),
+                                              child: Text(
+                                                chatData['timeStamp'],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    })
+                                : const SizedBox();
+                          });
+                    }),
+              ],
+            ),
           ),
         ),
       ),
